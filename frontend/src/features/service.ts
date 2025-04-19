586c9c8fb6251a2d5ab4a7e2b63c60f7eccdf057
RipplB <balrip.play@gmail.com>
2025-04-20 01:19:03 +0200

    try fix auth

--- frontend/src/features/service.ts
+++ frontend/src/features/service.ts
@@ -9,41 +9,41 @@ import {
   Register,
 } from "./models";
 
-const api = axios.create({
+const createApiClient = () => axios.create({
   baseURL: import.meta.env.VITE_API_URL as string,
 });
 
 async function registerUser(data: Register): Promise<void> {
-  await api.post("/auth/register", data);
+  await createApiClient().post("/auth/register", data);
 }
 
 async function loginUser(data: Login): Promise<LoginResponse> {
-  const response = await api.post("/auth/login", data);
+  const response = await createApiClient().post("/auth/login", data);
   return LoginResponse.parse(await response.data);
 }
 
 async function logoutUser(): Promise<void> {
-  await api.post("/auth/logout");
+  await createApiClient().post("/auth/logout");
 }
 
 async function fetchPhotos(params: PhotosQuery): Promise<PhotoListResponse> {
-  const response = await api.get("/photos", { params });
+  const response = await createApiClient().get("/photos", { params });
   return PhotoListResponse.parse(await response.data);
 }
 
 async function uploadPhoto(data: FormData): Promise<void> {
-  await api.post("/photos", data, {
+  await createApiClient().post("/photos", data, {
     headers: { "Content-Type": "multipart/form-data" },
   });
 }
 
 async function fetchPhotoById(photoId: string): Promise<Photo> {
-  const response = await api.get(`/photos/${photoId}`);
+  const response = await createApiClient().get(`/photos/${photoId}`);
   return Photo.parse(await response.data);
 }
 
 async function deletePhotoById(photoId: string): Promise<void> {
-  await api.delete(`/photos/${photoId}`);
+  await createApiClient().delete(`/photos/${photoId}`);
 }
 
 export const service = {
